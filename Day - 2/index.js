48b3f3aea2173fc737bf5441d48f8d05559ef09f
Densaravanan <sarvansaravanan1510@gmail.com>
2024-03-29 19:45:49 +0530

    test

---
+++ Day - 2/index.js
@@ -0,0 +1,15 @@
+console.log("Hello World!!");
+console.log("Welcome,FSDWD-T People.");
+
+var num1 = 100;
+var num2 = 500;
+var num3 = 50;
+var num4 = 50;
+var firstname = "Dens";
+var lastname = "Saravanan";
+
+console.log(num1 + num2);
+console.log(num3 - num4);
+
+console.log(firstname + lastname);
+console.log(firstname - lastname);
---
+++ Day - 5/ar-object-destructuring.js
@@ -0,0 +1,6 @@
+const numbersNew = [1,2,3,4,5];
+const [one,two,,,others] = numbersNew;
+console.log("one",one);
+console.log("two",two);
+console.log("others",others);
+
