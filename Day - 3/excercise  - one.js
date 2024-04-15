9eb380d088ea404a85fa8502f77f0357de6f01b9
Densaravanan <sarvansaravanan1510@gmail.com>
2024-04-15 14:11:13 +0530

    task3

---
+++ Day - 3/excercise  - one.js
@@ -0,0 +1,18 @@
+var age = 10;
+if (age >= 18) {
+    console.log( "you are eligible to vote.");
+} else {
+    console.log( "you are not eligile to vote.");
+}
+
+var speed = 70;
+if(speed < 30) {
+     console.log("the bike is slow Speed.");
+} else if(speed <= 80) {
+    console.log("the bike is average speed.");
+} else {
+ console.log("the bike is over speed.");
+}
+
+var num = 25;
+num % 2 === 0 ? console.log("EVEN") : console.log("ODD") ;
---
+++ Day - 3/index.js
@@ -0,0 +1,15 @@
+var x = 5 * 3/3;
+console.log(x);
+
+var x = 25 / 125 / 5
+console.log(x);
+
+var x = 5;
+var y = x++;
+console.log(x);
+console.log(y);
+
+var x = 5;
+var y = ++x;
+console.log(x);
+console.log(y);
